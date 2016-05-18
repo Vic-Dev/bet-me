a19f5d13022dcc6bad468ed37089acc208894388
Victoria Johns <vmichellej@gmail.com>
2016-05-18 18:20:19 -0400

    Add users table

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  has_secure_password
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160518213812) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password_digest"
+    t.string   "login_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
