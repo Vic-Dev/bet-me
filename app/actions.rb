d52ae3c2f628db7c4407d5bf9d2752101cf7aa06
Greg Markou <gregorymarkou@gmail.com>
2016-05-18 23:34:42 -0400

    Update nav bar layout

--- app/actions.rb
+++ app/actions.rb
@@ -1,10 +1,10 @@
 # Homepage (Root path)
-get '/' do
+get "/" do
   erb :index
 end
 
 get '/profile/:id' do
-  @user = User.find params[:id]
-  # @challenges = Challenge.all <<<< to specific user
+  @user = User.find(params[:id])
+  @challenges = @user.challenges
   erb :'user/profile'
 end
