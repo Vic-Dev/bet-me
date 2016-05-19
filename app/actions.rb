38292485a37b15c412921df89c589dabbd17ae79
Greg Markou <gregorymarkou@gmail.com>
2016-05-19 16:53:02 -0400

    Add all challenges page. Add routes to nav bar

--- app/actions.rb
+++ app/actions.rb
@@ -6,6 +6,8 @@ get '/user/profile' do
 end
 
 get '/user/profile/:id' do
+  @current_challenges = Challenge.where("end_time > ?", Time.current)
+  @expired_challenges = Challenge.where("start_time < ?", Time.current)
   @user = User.find(params[:id])
   @challenges = @user.challenges.order(end_time: :desc)
   erb :'user/profile'
@@ -15,12 +17,25 @@ end
 # challenges
 #============
 
-get '/challenges'
+get '/challenges' do
   @current_challenges = Challenge.where("end_time > ?", Time.current)
   @expired_challenges = Challenge.where("start_time < ?", Time.current)
+  erb :'challenges/index'
 end
 
 get '/challenges/:id' do
+  @user = current_user
+  @is_creator = Record.where("role = ? AND user_id = ?",'creator',@user.id)
+  @is_voter = Record.where("role = ? AND user_id = ?",'voter',@user.id)
   @challenge = Challenge.find(params[:id])
+  #@voter_result is number of TRUE votes
+  @true_votes = @challenge.users.where('vote_result = ?',true).count(:vote_result)
+  #subtract the creator becasue he cannot vote
+  @total_voters = @challenge.users.length - 1
+  @post_vote_result = (@true_votes >= @total_voters/2) ? true : false
   erb :'challenges/profile'
+end
+
+def doit
+  puts "hello"
 end
